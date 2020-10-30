3e46fa9deff3ecfa83f5ebf5c6adc11fceca05b0
suteki <752319034@qq.com>
2020-10-30 12:29:54 +0800

    commit

---
+++ js/voice.js
@@ -0,0 +1,7 @@
+var no = [{"jp":"萌","sc":"萌","voice":[{"jp":"ありがとうバイバイ","sc":"谢谢拜拜","file":"dao"}
+												,{"jp":"キラキラ","sc":"kirakira","file":"kirakira"}
+		]},{"jp":"悲鳴","sc":"怪叫","voice":[{"jp":"あびゃー","sc":"abia","file":"abia"}
+												,{"jp":"いやいやいやいや","sc":"不要不要不要不要","file":"iya"}
+		]},{"jp":"悪口","sc":"粗口","voice":[{"jp":"くそ","sc":"可恶","file":"kuso"}
+												,{"jp":"ド畜生やな","sc":"畜生","file":"chikusyou"}
+		]}]
