fc80c0012495e49097a78d7a14c06d7869b546de
hanayorisuteki <73635117+hanayorisuteki@users.noreply.github.com>
2020-10-30 14:07:01 +0800

    Update and rename js/voice.js to JS/voice.js

--- JS/voice.js
+++ JS/voice.js
@@ -1,7 +1,12 @@
 var no = [{"jp":"萌","sc":"萌","voice":[{"jp":"ありがとうバイバイ","sc":"谢谢拜拜","file":"dao"}
 												,{"jp":"キラキラ","sc":"kirakira","file":"kirakira"}
+												,{"jp":"リップロール","sc":"liproll","file":"chikusyou"}
 		]},{"jp":"悲鳴","sc":"怪叫","voice":[{"jp":"あびゃー","sc":"abia","file":"abia"}
 												,{"jp":"いやいやいやいや","sc":"不要不要不要不要","file":"iya"}
 		]},{"jp":"悪口","sc":"粗口","voice":[{"jp":"くそ","sc":"可恶","file":"kuso"}
 												,{"jp":"ド畜生やな","sc":"畜生","file":"chikusyou"}
+												,{"jp":"アホやろ","sc":"是不是傻","file":"chikusyou"}
+												,{"jp":"ドアホ","sc":"大笨蛋","file":"chikusyou"}
+												,{"jp":"死ねよ","sc":"去死吧","file":"chikusyou"}
 		]}]
+
